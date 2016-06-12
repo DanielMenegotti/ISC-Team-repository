f68b7d4feaf8114791c2e724b2f7aafd9fc2e2bd
DanielMenegotti <none@none>
2016-06-12 01:02:08 -0400

    You can now go to level 4, no minibosses floors 3 & 4, final floor not added yet.

--- Combat/Entities.py
+++ Combat/Entities.py
@@ -1,10 +1,10 @@
 class Entity():
     
-    def __init__(self, name = "Test", damage = 1, health = 1, armor = 0):
+    def __init__(self, name = "Test", damage = 1, health = 1, score = 0):
         self.name = name
         self.damage = damage
         self.health = health
-        self.armor = armor
+        self.score = score
     
  
         
@@ -18,11 +18,14 @@ easyogre = Entity("Ogre", 3, 8, 2)
 
 big_Rat = Entity("Big rat", 2, 5, 0)
 slime = Entity("Slime", 1, 5, 0)
-ogre = Entity("ogre", 3, 8, 2)
+ogre = Entity("Ogre", 3, 8, 2)
 
 hardbig_Rat = Entity("Big rat", 2, 3, 0)
 hardslime = Entity("Slime", 3, 8, 0)
 hardogre = Entity("Ogre", 4, 15, 2)
+
+miniBossCyclops = Entity("Cyclops", 3, 16, 0)
+miniBossFairy = Entity("Fairy",2 ,14, 0)
 
 character = Entity("Character", 2, 10, 0)
         
